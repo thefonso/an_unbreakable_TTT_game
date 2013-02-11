16aaad42d6b19011eab6b149c35725bc15b8830a
thefonso <alephmage@gmail.com>
2013-02-11 14:58:27 -0600

    starting to build player class

---
+++ lib/player.rb
@@ -0,0 +1,5 @@
+class Player
+  def human
+    p "human"
+  end
+end
---
+++ spec/player_spec.rb
@@ -0,0 +1,12 @@
+require 'player'
+
+describe 'Player class' do
+  it 'should be human or AI' do
+    player=Player.new
+    player.human.should == "human"
+  end
+
+  xit 'should ' do
+  end
+end
+
