8a2b3a1c555c7216c0830bfbf8f901cfa6e9a206
thefonso <alephmage@gmail.com>
2013-02-11 15:15:53 -0600

    what does a player do?

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,12 @@
 class Player
+  # what does a human do?
+  # a human exist
+  # a human plays a piece 
+  # a human places piece on the board, makes a move
+  #
   def human
-    p "human"
+    piece = 'x'
+    move = #read from command line
   end
+
 end
