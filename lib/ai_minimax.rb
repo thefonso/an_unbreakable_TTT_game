7aa8fcd4a050dd3a36aa75ff8414350d88cdfe57
thefonso <alephmage@gmail.com>
2013-06-18 15:46:33 -0500

    ply values were off...corrected now....may handle splits

--- lib/ai_minimax.rb
+++ lib/ai_minimax.rb
@@ -3,151 +3,176 @@ require_relative 'windetection'
 require 'pry'
 
 class Minmax 
-    include WinDetection
+  include WinDetection
 
-    attr_accessor :i
-    
-    def initialize
-      @i = 0
+  attr_accessor :i
+
+  def initialize
+    @i = 0
+  end
+
+  def minmax(board, player)
+    if board.grid == ["+", "+", "+", "+", "+", "+", "+", "+", "+"]
+      first_move
+    else
+      scores_hash = score_the_boards(board, player)
+
+      puts 'scores_hash '+scores_hash.to_s
+
+      max_hash = find_max_board(scores_hash)
+      puts 'max_hash '+max_hash.to_s
+      min_hash = find_min_board(scores_hash)
+      puts 'min_hash '+min_hash.to_s
+
+      answers = (max_hash.to_a - min_hash.to_a)
+      puts 'answers '+answers.to_s
+
+      intersect = Hash[*answers.flatten]
+      puts 'intersect '+intersect.to_s
+
+      max_move = intersect.select{|k,v| v == 'O'}.keys[0]
+      puts 'max_move '+max_move.to_s
+      min_move = intersect.select{|k,v| v == 'X'}.keys[0]
+      puts 'min_move '+min_move.to_s
+      return max_move
     end
+  end
 
-    def minmax(board, player)
-      if board.grid == ["+", "+", "+", "+", "+", "+", "+", "+", "+"]
-        first_move
+  def find_max_board(scores_hash)
+    max_board = scores_hash.max_by{|k,v| v}[0]
+    # p "Max board "+max_board.to_s
+    max_hash = {}
+    max_board.each_with_index{|item, index| max_hash[index] = item}
+
+    # p"max_hash "+max_hash.to_s
+    return max_hash
+  end
+
+  def find_min_board(scores_hash)
+    min_board = scores_hash.min_by{|k,v| v}[0]
+    # p "Min board "+min_board.to_s
+    min_hash = {}
+    min_board.each_with_index{|item, index| min_hash[index] = item}
+
+    # p"min_hash "+min_hash.to_s
+    return min_hash
+  end
+
+  def first_move
+    start_moves = [0, 2, 4, 6, 8].sample
+    return start_moves
+  end
+
+  def score_the_boards(board, player_symbol)
+    @count = 0
+    ply = 0
+    @i = 0
+    test_boards_hash = {}
+    test_boards_hash = generate_boards(board, player_symbol)
+    # p "the test_boards_hash "+test_boards_hash.to_s
+    scores_hash = {}
+    test_boards_hash.each do |_,myboard|
+
+      if three_in_a_row_win?(myboard, player_symbol) != nil 
+        if three_in_a_row_win?(myboard, 'O') == true
+          scores_hash[myboard] = (1000 + ply)  # plus infinity and ply level
+        elsif three_in_a_row_win?(myboard, 'X') == true
+          scores_hash[myboard] = (-1000 + ply) # minus infinity and ply level
+        else
+          # do I even need the draw boards?
+          scores_hash[myboard] = 0
+        end
       else
-        scores_hash = score_the_boards(board, player)
-        max_hash = find_max_board(scores_hash)
-        min_hash = find_min_board(scores_hash)
-        answers = (max_hash.to_a - min_hash.to_a)
-        intersect = Hash[*answers.flatten]
-        max_move = intersect.select{|k,v| v == 'O'}.keys[0]
-        min_move = intersect.select{|k,v| v == 'X'}.keys[0]
-        return max_move
+        puts 'D R A W'
+      end
+
+      ply -= 5
+    end
+    # p scores_hash
+    return scores_hash
+  end
+
+
+  def generate_boards(board, player)
+    board_array = board.grid
+    @virtual_board_hash ||= {}
+    # convert incoming board array into a hash 
+    board_to_hash = {}.compare_by_identity 
+    board_array.each_with_index{|k,v| board_to_hash[v] = k}
+
+    virtual_board_hash = board_to_hash.dup
+
+    # puts 'virtual_board_hash '+virtual_board_hash.to_s
+
+    empty_spaces = virtual_board_hash.select{ |k,v| v == '+' }.keys
+
+    # puts 'empty_spaces '+empty_spaces.to_s
+
+    empty_spaces.each do |space|
+
+      cloned_board = Board.new
+      cloned_board.grid = board.grid.clone
+
+      if player == "O"        
+        new_board = move_as_somebody(cloned_board, "X", space)
+        # puts 'new_board 1 '+new_board.to_s
+        new_player = "X"
+        @virtual_board_hash[@i] = new_board.grid
+      else
+        new_board = move_as_somebody(cloned_board, "O", space)
+        # puts 'new_board 2 '+new_board.to_s
+        new_player = "O"
+        @virtual_board_hash[@i] = new_board.grid
+      end
+      #convert hash into array
+      generate_boards(new_board, new_player)
+    end
+
+    # p "PING_PING"+ @virtual_board_hash.to_s
+    return @virtual_board_hash
+  end
+
+
+  def move_as_somebody(board, player, empty_space)
+    board.grid[empty_space] = player
+    @i+=1
+    return board
+  end
+
+  def draw?(grid)
+    grid.none? { |mark| mark == '+' }
+  end
+
+  def switch_player(player_symbol)
+    player_symbol == 'X' ? 'O' : 'X'
+  end
+
+  def score(board, player_symbol, position)
+    newboard = move_as_somebody(board, player_symbol, position)
+    opponent = switch_player(player_symbol)
+
+    if three_in_a_row_win?(newboard.grid, player_symbol)
+      return 1
+    elsif three_in_a_row_win?(newboard.grid, opponent)
+      return -1
+    elsif draw?(newboard.grid)
+      return 0
+    end
+
+    best_score = -1
+    empty_spaces = []
+    newboard.grid.each_with_index do |mark, index|
+      if mark == '+'
+        empty_spaces << index
       end
     end
 
-    def find_max_board(scores_hash)
-        max_board = scores_hash.max_by{|k,v| v}[0]
-        # # p"Max board "+max_board.to_s
-        max_hash = {}
-        max_board.each_with_index{|item, index| max_hash[index] = item}
-      
-        # p"max_hash "+max_hash.to_s
-        return max_hash
-    end
-
-    def find_min_board(scores_hash)
-        min_board = scores_hash.min_by{|k,v| v}[0]
-        # puts "Min board "+min_board.to_s
-        min_hash = {}
-        min_board.each_with_index{|item, index| min_hash[index] = item}
-      
-        # p"min_hash "+min_hash.to_s
-        return min_hash
-    end
-    
-    def first_move
-      start_moves = [0, 2, 4, 6, 8].sample
-    end
-
-    def score_the_boards(board, player_symbol)
-      @count = 0
-      ply = 0
-      @i = 0
-      test_boards_hash = {}
-      test_boards_hash = generate_boards(board, player_symbol)
-      scores_hash = {}
-      test_boards_hash.each do |_, board|
-        if three_in_a_row_win?(board, player_symbol) != nil 
-          if three_in_a_row_win?(board, 'O') == true
-            scores_hash[board] = (1000 - ply)
-          elsif three_in_a_row_win?(board, 'X') == true
-            scores_hash[board] = (-1000 + ply)
-          else
-            scores_hash[board] = 0
-          end
-        else
-          puts 'D R A W'
-        end
-
-        ply -= 5
+    empty_spaces.each do |space|
+      opponent_score = score(newboard,opponent,space)
+      if opponent_score * -1 > best_score
+        best_score = opponent_score * -1
       end
-      return scores_hash
     end
-
-
-    def generate_boards(board, player)
-      @virtual_board_hash ||= {}
-      virtual_board = board.dup
-      empty_spaces = []
-
-      virtual_board.grid.size.times do |empty|
-        if virtual_board.grid[empty] == '+'
-          empty_spaces << empty
-        end
-      end
-
-      empty_spaces.each do |space|
-        cloned_board = Board.new
-        cloned_board.grid = board.grid.clone
-
-        if player == "O"
-          new_board = move_as_somebody(cloned_board, "X", space)
-          new_player = "X"
-          @virtual_board_hash[@i] = new_board.grid
-        else
-          new_board = move_as_somebody(cloned_board, "O", space)
-          new_player = "O"
-          @virtual_board_hash[@i] = new_board.grid
-        end
-
-        generate_boards(new_board, new_player)
-      end
-
-      return @virtual_board_hash
-    end
-
-
-    def move_as_somebody(board, player, empty_space)
-      board.grid[empty_space] = player
-      @i+=1
-      return board
-    end
-
-    def draw?(grid)
-      grid.none? { |mark| mark == '+' }
-    end
-
-    def switch_player(player_symbol)
-      player_symbol == 'X' ? 'O' : 'X'
-    end
-
-    def score(board, player_symbol, position)
-      newboard = move_as_somebody(board, player_symbol, position)
-      opponent = switch_player(player_symbol)
-
-      if three_in_a_row_win?(newboard.grid, player_symbol)
-        return 1
-      elsif three_in_a_row_win?(newboard.grid, opponent)
-        return -1
-      elsif draw?(newboard.grid)
-        return 0
-      end
-
-      best_score = -1
-      empty_spaces = []
-      newboard.grid.each_with_index do |mark, index|
-        if mark == '+'
-          empty_spaces << index
-        end
-      end
-
-      empty_spaces.each do |space|
-        opponent_score = score(newboard,opponent,space)
-        if opponent_score * -1 > best_score
-          best_score = opponent_score * -1
-        end
-      end
-      return best_score
-    end
+    return best_score
+  end
 end
--- spec/ai_minimax_spec.rb
+++ spec/ai_minimax_spec.rb
@@ -10,18 +10,12 @@ describe 'Pseudo_Minimax_Ai' do
     @minimax = Minmax.new
     @minimax.i = 0
 
-    @board.grid[0] = "O"
-    @board.grid[1] = "O"
-    @board.grid[2] = "X"
-    @board.grid[3] = "X"
-    @board.grid[4] = "X"
-    @board.grid[5] = "+"
-    @board.grid[6] = "+"
-    @board.grid[7] = "+"
-    @board.grid[8] = "+"
+    @board.grid=["O","O","X",
+                 "X","X","+",
+                 "+","+","+"]
   end
 
-  describe 'minimax' do
+  describe 'minmax' do
     it 'should return an integer' do
       @minimax.minmax(@board, @player).is_a?(Integer)
     end
@@ -32,16 +26,11 @@ describe 'Pseudo_Minimax_Ai' do
     end
 
     it 'should defend against a split' do
-      @board.grid[0] = "X"
-      @board.grid[1] = "O"
-      @board.grid[2] = "+"
-      @board.grid[3] = "+"
-      @board.grid[4] = "O"
-      @board.grid[5] = "+"
-      @board.grid[6] = "+"
-      @board.grid[7] = "X"
-      @board.grid[8] = "+"
-      @minimax.minmax(@board, @player).should == 6
+      @board.grid=["+","O","X",
+                   "+","O","+",
+                   "+","X","+"]
+      
+      @minimax.minmax(@board, @player).should == 5
     end
   end
 
@@ -60,35 +49,32 @@ describe 'Pseudo_Minimax_Ai' do
       @minimax = Minmax.new
       @minimax.i = 0
 
-      @board.grid[0] = "X"
-      @board.grid[1] = "O"
-      @board.grid[2] = "O"
-      @board.grid[3] = "O"
-      @board.grid[4] = "O"
-      @board.grid[5] = "X"
-      @board.grid[6] = "X"
-      @board.grid[7] = "+"
-      @board.grid[8] = "+"
+      @board.grid=["X","O","O",
+                   "O","O","X",
+                   "X","+","+"]
 
       boards = @minimax.generate_boards(@board, @player)
+      
       boards.values.include?(["X","O","O","O","O","X","X","X","+"]).should be_true
       boards.values.include?(["X","O","O","O","O","X","X","X","O"]).should be_true
       boards.values.include?(["X","O","O","O","O","X","X","+","X"]).should be_true
       boards.values.include?(["X","O","O","O","O","X","X","O","X"]).should be_true
     end
   end
+ 
+  describe 'move_as_somebody(board, player, empty_space)' do
+    empty_space = 2
+
+    it 'should return new board Array'do
+     @minimax.move_as_somebody(@board, @player, empty_space).grid.should be_a_kind_of(Array)
+    end
+  end
 
   describe 'score function' do
     it 'scores a draw' do
-      @board.grid[0] = "X"
-      @board.grid[1] = "O"
-      @board.grid[2] = "O"
-      @board.grid[3] = "O"
-      @board.grid[4] = "O"
-      @board.grid[5] = "X"
-      @board.grid[6] = "X"
-      @board.grid[7] = "X"
-      @board.grid[8] = "+"
+      @board.grid=["X","O","O",
+                   "O","O","X",
+                   "X","X","+"]
 
       minmax = Minmax.new
       minmax.score(@board, 'O', 8).should == 0
@@ -96,15 +82,9 @@ describe 'Pseudo_Minimax_Ai' do
 
     it 'scores a win' do
       board = Board.new
-      board.grid[0] = "X"
-      board.grid[1] = "O"
-      board.grid[2] = "O"
-      board.grid[3] = "O"
-      board.grid[4] = "O"
-      board.grid[5] = "X"
-      board.grid[6] = "X"
-      board.grid[7] = "+"
-      board.grid[8] = "X"
+      board.grid=["X","O","O",
+                  "O","O","X",
+                  "X","+","X"]
 
       minmax = Minmax.new
       minmax.score(board, 'O', 7).should == 1
@@ -112,15 +92,9 @@ describe 'Pseudo_Minimax_Ai' do
 
     it 'scores a lose' do
       board = Board.new
-      board.grid[0] = "X"
-      board.grid[1] = "O"
-      board.grid[2] = "O"
-      board.grid[3] = "O"
-      board.grid[4] = "O"
-      board.grid[5] = "X"
-      board.grid[6] = "X"
-      board.grid[7] = "O"
-      board.grid[8] = "+"
+      board.grid=["X","O","O",
+                  "O","O","X",
+                  "X","O","+"]
 
       minmax = Minmax.new
       minmax.score(board, 'X', 8).should == -1
@@ -128,15 +102,9 @@ describe 'Pseudo_Minimax_Ai' do
 
     it 'scores two moves left for a win' do
       board = Board.new
-      board.grid[0] = "+"
-      board.grid[1] = "O"
-      board.grid[2] = "X"
-      board.grid[3] = "+"
-      board.grid[4] = "O"
-      board.grid[5] = "+"
-      board.grid[6] = "X"
-      board.grid[7] = "+"
-      board.grid[8] = "+"
+      board.grid=["+","O","X",
+                  "+","O","+",
+                  "X","+","+"]
 
       minmax = Minmax.new
       minmax.score(board, 'X', 0).should == -1
@@ -144,13 +112,5 @@ describe 'Pseudo_Minimax_Ai' do
     end
   end
 
-  describe 'move_as_somebody(board, player, empty_space)' do
-    empty_space = 2
-
-    it 'should return new board Array'do
-     @minimax.move_as_somebody(@board, @player, empty_space).grid.should be_a_kind_of(Array)
-    end
-
-  end
 end
 
