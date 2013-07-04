0fdd524fdc4d48e1e206eca5e96344990f17cd85
thefonso <alephmage@gmail.com>
2013-07-04 17:53:48 -0500

    progress save - should I move my recursion focus?

--- lib/ai_minimax.rb
+++ lib/ai_minimax.rb
@@ -22,12 +22,12 @@ class Minmax
 
   def minmax(board, player)
 
-    opponent = switch_player(player) 
     return first_move if first_move?(board)
+    i=0
+    ply_depth = 0
+    opponent = switch_player(player)
+    clonedboards_hash = Hash.new 
 
-    depth = 1
-    #i=0
-    #clonedboards_hash = Hash.new 
 
     board_hash = Hash[(0...board.grid.size).zip board.grid]
     empty_spaces = board_hash.select{ |k,v| v == '+' }.keys 
@@ -35,66 +35,70 @@ class Minmax
     cloned_board = Board.new
     cloned_board = board.grid.clone
     # binding.pry
-    # scores_hash = Hash.new
 
     if draw?(cloned_board)
       return 0
     else
+
       empty_spaces.each do |space|
+
+        cloned_again_board = board.grid.clone
+        cloned_again_board[space] = player
+        clonedboards_hash[i] = cloned_again_board
+
+        i += 1 
+
+        enemy_board = board.grid.clone
+        enemy_board[space] = opponent
+        clonedboards_hash[i] = enemy_board
+        #binding.pry
+
+        i += 1 
+
         if score_a_move(cloned_board, player, space) == 1
           return space
         elsif score_a_move(cloned_board, opponent, space) == 1
           return space
+        else
         end
+
+      end
+      ply_depth += 1
+
+      #binding.pry
+      clonedboards_hash.each do |key,nextlevel|
+        nextlevel_board = nextlevel
+        #TODO
+        # How do I get this recursion going?
+        # parts of equation...ply_depth, score_a_move, clonedboards_hash
+        #
+        # Q.what is base case?
+        # A.score_a_move() == 1
+        minmax(nextlevel_board,player)
+
       end
     end
-
   end
 
 
   def score_a_move(board, player_symbol, empty_space)
+    #TODO
+    #move bulk of code here...
+    #...recursive this method
+    #
     opponent = switch_player(player_symbol) 
-    # depth = 1
-    i=0
-    clonedboards_hash = Hash.new 
-
+    #binding.pry
     cloned_again_board = board.clone
     cloned_again_board[empty_space] = player_symbol
-    clonedboards_hash[i] = cloned_again_board
-
-    i += 1 
-
-    enemy_board = board.clone
-    enemy_board[empty_space] = opponent
-    clonedboards_hash[i] = enemy_board
-    #binding.pry
-
-    i += 1 
 
     if three_in_a_row_win?(cloned_again_board, player_symbol)
-      # scores_hash[1] = space
       return 1
-
-    elsif three_in_a_row_win?(enemy_board, opponent)
-      # scores_hash[-1] = space
+    elsif three_in_a_row_win?(cloned_again_board, opponent)
       return -1
+    else
+      return 0
     end
 
-
-    #depth += 1
-
-    #TODO
-    #How do I get this recursion going?
-
-    # if scores_hash.max != nil
-    #   #p "SCORES_HASH"+scores_hash.to_s+"  DEPTH "+depth.to_s
-    #   return scores_hash.max.last
-    # else
-    #   clonedboards_hash.first do |key,secondboard|
-    #     #p "SECONDBOARD"+secondboard.to_s
-    #     score_a_move(secondboard, player_symbol, empty_space)
-    #   end
-    # end
   end
 
   def move_as_somebody(board, player, empty_space)
--- spec/ai_minimax_spec.rb
+++ spec/ai_minimax_spec.rb
@@ -17,12 +17,12 @@ describe 'Pseudo_Minimax_Ai' do
 
   describe 'minmax' do
 
-    it 'should take optimal first move' do
+    xit 'should take optimal first move' do
       @board.stub(:available_spaces).and_return(["+", "+", "+", "+", "+", "+", "+", "+", "+"])
       @minimax.first_move.should satisfy {|s| [0,2,4,6,8].include?(s)}
     end
 
-    it 'should give a winning move' do
+    xit 'should give a winning move' do
       @board.grid=["X","+","+",
                    "+","+","+",
                    "+","+","X"]
@@ -30,7 +30,7 @@ describe 'Pseudo_Minimax_Ai' do
       @minimax.minmax(@board, @player).should == 4
     end
 
-    it 'should return a blocking move' do
+    xit 'should return a blocking move' do
       @board.grid=["O","+","+",
                    "+","O","+",
                    "+","+","+"]
@@ -38,7 +38,7 @@ describe 'Pseudo_Minimax_Ai' do
      @minimax.minmax(@board, @player).should == 8
     end
 
-    it 'should recognize a draw' do
+    xit 'should recognize a draw' do
       @board.grid=["O","O","X",
                    "X","X","O",
                    "O","X","X"]
@@ -46,10 +46,10 @@ describe 'Pseudo_Minimax_Ai' do
      @minimax.minmax(@board, @player).should == 0
     end
 
-    xit 'should defend against a split' do
-      @board.grid=["X","O","+",
-                   "+","O","+",
-                   "+","X","+"]
+    it 'should defend against a split' do
+      @board.grid=["O","X","+",
+                   "+","X","+",
+                   "+","O","+"]
       
       @minimax.minmax(@board, @player).should == 3
     end
@@ -78,39 +78,19 @@ describe 'Pseudo_Minimax_Ai' do
 
     xit 'should defend a split' do
       empty_space = 0
-      @board.grid=["X","O","+",
-                   "+","O","+",
-                   "+","X","+"]
+      @board=["X","O","+",
+              "+","O","+",
+              "+","X","+"]
 
      @minimax.score_a_move(@board, @player, empty_space).should == 3
     end
   end
 
-  describe 'generate_boards(board, player)' do
-    xit 'holds all the boards' do
-      @player = "O"
-      @count = 0
-      @board = Board.new
-      @minimax = Minmax.new
-      @minimax.i = 0
 
-      @board.grid=["X","O","O",
-                   "O","O","X",
-                   "X","+","+"]
-
-      boards = @minimax.generate_boards(@board, @player)
-      
-      boards.values.include?(["X","O","O","O","O","X","X","X","+"]).should be_true
-      boards.values.include?(["X","O","O","O","O","X","X","X","O"]).should be_true
-      boards.values.include?(["X","O","O","O","O","X","X","+","X"]).should be_true
-      boards.values.include?(["X","O","O","O","O","X","X","O","X"]).should be_true
-    end
-  end
- 
   describe 'move_as_somebody(board, player, empty_space)' do
     empty_space = 2
 
-    xit 'should return new board Array'do
+    it 'should return new board Array'do
      @minimax.move_as_somebody(@board, @player, empty_space).grid.should be_a_kind_of(Array)
     end
   end
