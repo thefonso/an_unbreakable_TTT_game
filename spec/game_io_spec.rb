69a559115f02afdfa1ed25fd157a90aa7033805c
thefonso <alephmage@gmail.com>
2013-07-17 19:18:34 -0500

    refactored test in gameio spec

--- spec/game_io_spec.rb
+++ spec/game_io_spec.rb
@@ -4,24 +4,44 @@ require 'board'
 
 describe GameIO do
   before(:each) do
-    @gameio = GameIO.new
-    @board  = Board.new
+    @board     = Board.new
+    test_in   = StringIO.new("some test input\n")
+    test_out  = StringIO.new
+    @test_io   = GameIO.new(test_in, test_out)
   end
 
-  context 'welcome_message' do
+  context 'welcome message' do
     it 'should display a welcome message' do
-      test_in   = StringIO.new("some test input\n")
-      test_out  = StringIO.new
-      test_io   = GameIO.new(test_in, test_out)
+      @test_io.welcome_message
+      @test_io.game_output.string.should == "Hey, welcome to my game. Get ready to be defeated\n"
+    end
+  end
 
-      test_io.welcome_message
-      test_io.game_output.string.should == "Hey, welcome to my game. Get ready to be defeated\n"
+  context 'winner message' do
+    it 'should display a winner message' do
+      @test_io.winner_message
+      @test_io.game_output.string.should == "Human has won!\n"
+    end
+  end
+
+  context 'draw message' do
+    it 'should display a game draw message' do
+      @test_io.draw_message
+      @test_io.game_output.string.should == "The game is a DRAW\n"
+    end
+  end
+
+  context 'game over message' do
+    it 'should display a game over message' do
+      @test_io.game_over_message
+      @test_io.game_output.string.should == "Game Over\n"
     end
   end
 
   context 'draw_three_by_three_board' do
-    xit 'should display the board on standard output' do
-      @gameio.draw_three_by_three(@board).should == <<-EOF.gsub(/^ {6}/, '')
+    it 'should display the board on standard output' do
+      @test_io.draw_three_by_three(@board)
+      @test_io.game_output.string.should == <<-EOF.gsub(/^ {6}/, '')
 
        + | + | +
       -----------
@@ -31,6 +51,5 @@ describe GameIO do
       EOF
     end
   end
-
 
 end
