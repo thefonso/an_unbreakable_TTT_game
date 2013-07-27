62b9c20fe79e339336c268e1bfd0601deffbeeda
thefonso <alephmage@gmail.com>
2013-07-27 16:53:39 -0500

    removed some orig files

--- lib/game_io.rb
+++ lib/game_io.rb
@@ -25,7 +25,6 @@ class GameIO
   def welcome_message 
     output "Hey, welcome to my game. Get ready to be defeated" 
   end
-  #TODO who won? || draw?
 
   def winner_message(player_symbol)
     output "#{player_symbol} has won!"
